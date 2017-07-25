1cea4619941d6b4210b2bdc3f0dfd56f49e86c91
Arnav Gupta <arnav@codingblocks.com>
2017-07-25 18:59:12 +0530

    readme and badges
    [skip ci]
    
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ README.md
@@ -0,0 +1,15 @@
+# mockstorage
+A node module (with full Typescript support) to emulate localStorage 
+and/or sessionStorage outside the browser.  
+(A major use case being unit tests)
+
+[![Build Status](https://travis-ci.org/championswimmer/mockstorage.svg?branch=master)](https://travis-ci.org/championswimmer/mockstorage)
+[![codecov](https://codecov.io/gh/championswimmer/mockstorage/branch/master/graph/badge.svg)](https://codecov.io/gh/championswimmer/mockstorage)
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/831494601a6747ca85d7ea74514d642c)](https://www.codacy.com/app/championswimmer/mockstorage?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=championswimmer/mockstorage&amp;utm_campaign=Badge_Grade)
+[![codebeat badge](https://codebeat.co/badges/1af9063e-301b-47e1-a94a-03d21e73fa83)](https://codebeat.co/projects/github-com-championswimmer-mockstorage-master)
+
+
+## LICENSE
+This project is licensed under the MIT license (a copy of which is part of the project)
+
+Copyright (c) 2017, Arnav Gupta
--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,17 @@ export class MockStorage implements Storage {
   [index: number]: string;
   private store: any = {}
 
+  /**
+   * Function to get number of keys in store
+   * @returns {number} number of keys in the store
+   */
   get length (): number {
     return Object.keys(this.store).length
   }
 
+  /**
+   * Function to clear the entire contents of the store
+   */
   public clear (): void {
     for (const key in this.store) {
       if (this.store.hasOwnProperty(key)) {
@@ -15,18 +22,38 @@ export class MockStorage implements Storage {
     }
   }
 
+  /**
+   * Fetch an item from the store
+   * @param {string} key
+   * @returns {string | any} the data on that key
+   */
   public getItem (key: string): string | any {
     return this.store[key]
   }
 
+  /**
+   * Get a key by it's index in the store
+   * (The order of keys are set by when they are first created)
+   * @param {number} index
+   * @returns {string | any} name of the key
+   */
   public key (index: number): string | any {
     return Object.keys(this.store)[index]
   }
 
+  /**
+   * Delete an item from the store
+   * @param {string} key
+   */
   public removeItem (key: string): void {
     delete this.store[key]
   }
 
+  /**
+   * Add an item to the store
+   * @param {string} key
+   * @param {string} data (remember to stringify it, if it's not string)
+   */
   public setItem (key: string, data: string): void {
     this.store[key] = data
   }
